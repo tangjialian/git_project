e836c51f4c694bef00db08f8a9d9f423d097c7f5
tangjialian <1849783734@qq.com>
2019-05-07 17:20:36 +0800

    第一次提交文件

---
+++ student_pro/src/com/nnxy/model/Student.java
@@ -0,0 +1,33 @@
+package com.nnxy.model;
+
+public class Student {
+	private int id;
+	private String username;
+	private int sex;
+	private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+
+}
